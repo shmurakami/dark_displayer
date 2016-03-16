94d06b3adfcf1818928a7ddfcc254b22f4d8993f
shmurakami <shmurakami1104@gmail.com>
2016-03-16 22:34:58 +0900

    ignore mouse event
    enable maximize

--- app/electron.js
+++ app/electron.js
@@ -6,8 +6,6 @@ const BrowserWindow = require('browser-window');
 let mainWindow = null;
 
 let windowStyle = {
-    width: 1900,
-    height: 1500,
     transparent: true,
     frame: false,
     alwaysOnTop: true
@@ -21,6 +19,8 @@ app.on('window-all-closed', () => {
 app.on('ready', () => {
     mainWindow = new BrowserWindow(windowStyle);
     mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.setIgnoreMouseEvents(true);
+    mainWindow.maximize();
 
     mainWindow.on('closed', () => {
         mainWindow = null;
