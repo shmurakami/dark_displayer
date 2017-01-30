82faf2edb32c81eb1407b514d80da3d360972069
Murakami.Shunsuke <murakami.shunsuke@aainc.co.jp>
2017-01-30 17:44:46 +0900

    support new electron version

--- app/electron.js
+++ app/electron.js
@@ -1,7 +1,8 @@
 'use strict';
 
-const app = require('app');
-const BrowserWindow = require('browser-window');
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow = null;
 
